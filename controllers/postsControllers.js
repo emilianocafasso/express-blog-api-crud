ef1dd73834ad7ea577c2f626e5c3e66bf2653ee1
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-12-01 01:31:52 +0100

    Add posts controller and initial post data

---
+++ controllers/postsControllers.js
@@ -0,0 +1,19 @@
+const index = (req, res) => {
+    res.send('Lista dei post')
+}
+
+const show = (req, res) => {
+    res.send('Info del post ' + req.params.id)
+}
+
+const store = (req, res) => {
+    res.send('Crea nuovo post')
+}
+
+const update = (req, res) => {
+    res.send('Modifica del post ' + req.params.id)
+}
+
+const destroy = (req, res) => {
+    res.send('Elimina post ' + req.params.id)
+}
