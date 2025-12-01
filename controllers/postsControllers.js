0610cd384fd7a308de1c03c3e3f8756bb153bbe3
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-12-02 00:43:16 +0100

    add store functionality in posts controller

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -52,7 +52,32 @@ const destroy = (req, res) => {
 
 // STORE - Creazione nuovo post
 const store = (req, res) => {
-    res.send('Creazione nuovo post')
+    console.log('Richiesta ricevuta: POST /posts')
+    console.log('Dati ricevuti nel body: ', req.body);
+
+
+    if (!req.body) {
+        log('Errore: req.body undefined')
+    }
+
+    // genero nuovo id
+    const newId = posts.length + 1;
+
+    // creo il post
+    const newPost = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags
+    }
+    // aggiungo all'array
+    posts.push(newPost)
+
+    console.log(`Nuovo post creato con ID: ${newId}`)
+
+    // restituisco la risposta
+    res.status(201).json(newPost)
 }
 
 // UPDATE - Modifica del singolo post 
