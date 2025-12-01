d8938661afdae401270a7ff25a6b92062521aef4
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-12-01 01:38:28 +0100

    include new routes to app.js

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const port = 3000
 
 
 app.use(express.static('public'))
-
 app.use(express.json())
+
+const postsRouter = require('./routers/posts')
+
+app.use('/posts', postsRouter)
 
 app.get('/', (req, res) => {
     res.send("Server")
