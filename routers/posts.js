2cc7f35c315ef67fb173a3e05789c33a1090484f
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-12-01 01:36:36 +0100

    associate routes to  controller functions

--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const postController = require("../controllers/postsControllers")
+
+router.get('/', postController.index)
+
+router.get('/:id', postController.show)
+
+router.get('/', postController.store)
+
+router.get('/:id', postController.update)
+
+router.get('/:id', postController.destroy)
+
+module.exports = router
